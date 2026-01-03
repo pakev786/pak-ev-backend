8e3d49944f6a9342da30ea8eb548a99491fb564f
trackmyhomeschool <Chanelle@trackmyhomeschool.com>
2026-01-03 20:04:03 +0500

    uploads changes

--- server.js
+++ server.js
@@ -44,7 +44,8 @@ if (process.env.EMAIL_USER && process.env.EMAIL_PASS) {
 }
 console.log("------------------------------------------------");
 
-const uploadsPath = path.join(__dirname, 'uploads');
+// const uploadsPath = path.join(__dirname, 'uploads');
+const uploadsPath = '/uploads';
 // Ensure upload directory exists
 import fs from 'fs';
 if (!fs.existsSync(uploadsPath)){
